17c4fad8e3c0b9ea6a9855db3f0c582dad5ca9cd
INXL\chacad <chaegie.valenciacadungog@avensia.com>
2025-05-12 06:43:07 +0800

    update facebook link

--- data/ClientInformation.tsx
+++ data/ClientInformation.tsx
@@ -3,6 +3,6 @@ export const ClientInformation = {
     contact2: '0494 368 354',
     address: '459R Redbank Road, Packers Camp, QLD 4865',
     mapLink: 'https://www.google.com/maps?ll=-17.011212,145.797208&z=16&t=m&hl=en&gl=AU&mapclient=embed&q=459+Redbank+Rd+Packers+Camp+QLD+4865',
-    facebook: 'https://www.facebook.com/cairns.boatyard',
+    facebook: 'https://facebook.com/profile.php?id=61576230238155',
     email: 'admin@cairnsboatstorage.com.au',
 }
