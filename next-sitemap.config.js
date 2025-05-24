ca14e1dbf0e675bf3e83b17823e002bcef038f82
Anthony Sevilla <SevillAnthonyy@gmail.com>
2025-05-24 13:47:47 +0800

    Added SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.cairnsboatstorage.com.au/', // 🔁 replace with your actual domain
+  generateRobotsTxt: true,
+};
