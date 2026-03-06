077bfccd98d3c3cd86b6570fe584cff171cfea8e
Anthony Sevilla <SevillAnthonyy@gmail.com>
2026-03-06 10:00:21 +0800

    Updated email

--- data/ClientInformation.tsx
+++ data/ClientInformation.tsx
@@ -4,5 +4,6 @@ export const ClientInformation = {
     address: '459R Redbank Road, Packers Camp, QLD 4865',
     mapLink: 'https://www.google.com/maps?ll=-17.011212,145.797208&z=16&t=m&hl=en&gl=AU&mapclient=embed&q=459+Redbank+Rd+Packers+Camp+QLD+4865',
     facebook: 'https://facebook.com/profile.php?id=61576230238155',
-    email: 'admin@cairnsboatstorage.com.au',
+    // email: 'admin@cairnsboatstorage.com.au',
+    email: 'cairnsboatstorage@gmail.com'
 }
